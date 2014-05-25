3ce7c62f11e176198779d966334d50cb30343ae6
kazumichi moromizato <k.moromizato@gmail.com>
2014-05-25 12:20:51 +0900

    本番DB設定

--- cake/app/Config/Const/system.php
+++ cake/app/Config/Const/system.php
@@ -1,12 +1,8 @@
 <?php
 
-if(env('HTTP_HOST') == 'www.padule.me' || env('HTTP_HOST') == 'padule.me') {
+if(env('HTTP_HOST') == 'api.cospradar.padule.me') {
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-} else if(env('HTTP_HOST') == 'ec2-54-248-64-73.ap-northeast-1.compute.amazonaws.com' || env('HTTP_HOST') == 'ec2-54-248-64-73.ap-northeast-1.compute.amazonaws.com'){
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-
 } else {
     define('DB_USER', 'mokyu');
     define('DB_PASSWORD', '');
