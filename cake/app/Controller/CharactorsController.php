1321b1e3086700ad5349c5ce199a8ac004be2ad9
kazumichi moromizato <k.moromizato@gmail.com>
2014-06-15 14:45:17 +0900

    charactorsテーブルにis_enabled  trueのものが追加されたら、それ以外はfalseにする

--- cake/app/Controller/CharactorsController.php
+++ cake/app/Controller/CharactorsController.php
@@ -17,5 +17,42 @@ class CharactorsController extends ApiController {
                 ));
     }
 
+        public function add() {
+
+        if(!isset($this->request->data['is_enabled'])){
+           $this->request->data['is_enabled'] = true;
+        }
+        if($this->request->data['is_enabled']) {
+            $updateConditions = array(
+                'user_id' => $this->request->data['user_id'],
+            );
+        }
+
+        $this->{$this->modelClass}->updateAll(array('is_enabled' => false),$updateConditions);
+        //画像の保存あり
+        if(isset($_FILES['image']['tmp_name'])){
+
+            $image = $_FILES['image'];
+            $this->{$this->modelClass}->save($this->request->data);
+            //画像保存先のパス
+            $path = WWW_ROOT . 'userdata' . DS . $this->modelClass . DS . $this->{$this->modelClass}->id;
+            move_uploaded_file($image['tmp_name'], $path);
+            $this->request->data['image'] = 'http://' . env('HTTP_HOST') . '/userdata/' . $this->modelClass . '/' . $this->{$this->modelClass}->id;
+            if ($this->{$this->modelClass}->save($this->request->data)) {
+                $response = $this->{$this->modelClass}->read();
+            }
+        } else {
+            if ($this->{$this->modelClass}->save($this->request->data)) {
+                $response = $this->{$this->modelClass}->read();
+            }
+        }
+
+        $this->set(
+                array(
+                    'response'   => $response,
+                    '_serialize' => 'response'
+                ));
+    }
+
 
 }
